076ba3bdd1d53272e49070cad02b6b32c0b0ed03
Gabor Szilagyi2 <gabor_szilagyi2@epam.com>
2020-02-26 10:36:41 +0100

    initial commit

---
+++ src/components/Timer/Timer.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import * as moment from "moment";
+
+export default class componentName extends Component {
+  state = {
+    time: 0,
+    isStarted: false,
+    start: 0
+  };
+
+  startTimer = () => {
+    this.setState({
+      isStarted: true,
+      time: this.state.time,
+      start: this.state.time
+    });
+    this.timer = setInterval(
+      () =>
+        this.setState({
+          time: this.state.start++
+        }),
+      1000
+    );
+  };
+
+  stopTimer = () => {
+    this.setState({ isStarted: false });
+    clearInterval(this.timer);
+  };
+
+  resetTimer = () => {
+    this.setState({ time: 0, isStarted: false });
+  };
+
+  render() {
+    return (
+      <>
+        <div className="timer">
+          <h4>
+            {moment()
+              .hour(0)
+              .minute(0)
+              .second(this.state.time)
+              .format("HH : mm : ss")}
+          </h4>
+          <button onClick={this.startTimer}>start</button>
+          <button onClick={this.stopTimer}>stop</button>
+          <button onClick={this.startTimer}>resume</button>
+          <button onClick={this.resetTimer}>reset</button>
+        </div>
+      </>
+    );
+  }
+}
