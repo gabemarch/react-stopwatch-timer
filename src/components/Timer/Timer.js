e1d6ec23e22771aba901683c1c56aefcfa0c0e7d
Gabor SZILAGYI <szilagyi_gabor@outlook.com>
2020-03-01 10:22:02 +0100

    minor changes in state

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import * as moment from "moment";
 
-export default class componentName extends Component {
+export default class Timer extends Component {
   state = {
     time: 0,
     isStarted: false,
@@ -14,41 +14,45 @@ export default class componentName extends Component {
       time: this.state.time,
       start: this.state.time
     });
+
     this.timer = setInterval(
       () =>
         this.setState({
-          time: this.state.start++
+          time: this.state.time + 1
         }),
       1000
     );
   };
 
   stopTimer = () => {
-    this.setState({ isStarted: false });
+    this.setState({
+      isStarted: false
+    });
     clearInterval(this.timer);
   };
 
   resetTimer = () => {
-    this.setState({ time: 0, isStarted: false });
+    this.setState({
+      time: 0,
+      isStarted: false
+    });
   };
 
   render() {
     return (
-      <>
-        <div className="timer">
-          <h4>
-            {moment()
-              .hour(0)
-              .minute(0)
-              .second(this.state.time)
-              .format("HH : mm : ss")}
-          </h4>
-          <button onClick={this.startTimer}>start</button>
-          <button onClick={this.stopTimer}>stop</button>
-          <button onClick={this.startTimer}>resume</button>
-          <button onClick={this.resetTimer}>reset</button>
-        </div>
-      </>
+      <div className="timer">
+        <h4>
+          {moment()
+            .hour(0)
+            .minute(0)
+            .second(this.state.time)
+            .format("HH : mm : ss")}
+        </h4>
+        <button onClick={this.startTimer}> start </button>
+        <button onClick={this.stopTimer}> stop </button>
+        <button onClick={this.startTimer}> resume </button>
+        <button onClick={this.resetTimer}> reset </button>
+      </div>
     );
   }
 }
